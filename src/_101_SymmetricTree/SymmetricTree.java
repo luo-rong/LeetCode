f78bcb33547607a0cb508855a213a88eb763e9ba
luorong_i <luorong_i@didichuxing.com>
2019-06-20 19:15:17 +0800

    101_对称二叉树（1AC 无题解）

---
+++ src/_101_SymmetricTree/SymmetricTree.java
@@ -0,0 +1,27 @@
+package _101_SymmetricTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class SymmetricTree {
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetric(root.left, root.right);
+    }
+}
