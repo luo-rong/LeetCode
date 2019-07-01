e04f2018c5d32fe7f289bc1bc7d1261acf7fe348
luorong_i <luorong_i@didichuxing.com>
2019-07-01 12:52:08 +0800

    206_反转链表

--- src/_206_ReverseLinkedList/ReverseLinkedList.java
+++ src/_206_ReverseLinkedList/ReverseLinkedList.java
@@ -1,4 +1,6 @@
-public class ReverseList {
+package _206_ReverseLinkedList;
+
+public class ReverseLinkedList {
     public class ListNode {
         int val;
         ListNode next;
@@ -9,7 +11,9 @@ public class ReverseList {
     }
 
     public ListNode reverseList1(ListNode head) {
-        if (head == null || head.next == null) return head;
+        if (head == null || head.next == null) {
+            return head;
+        }
         ListNode boot = reverseList(head.next);
         head.next.next = head;
         head.next = null;
@@ -17,7 +21,9 @@ public class ReverseList {
     }
 
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) return head;
+        if (head == null || head.next == null) {
+            return head;
+        }
         ListNode current = head;
         ListNode prev = null;
         while (current != null) {
