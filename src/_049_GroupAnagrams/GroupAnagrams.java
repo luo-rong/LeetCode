7b7517432e3687a4d2c3f73fed0d9f11f0778655
luorong_i <luorong_i@didichuxing.com>
2019-07-09 16:58:21 +0800

    049_字母异位词分组（1AC 无题解）

---
+++ src/_049_GroupAnagrams/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package _049_GroupAnagrams;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    private String getStrHashCode(String string) {
+        int[] codes = new int[26];
+        for (int i = 0; i < string.length(); ++i) {
+            codes[string.charAt(i) - 'a']++;
+        }
+        StringBuilder hashCode = new StringBuilder();
+        for (int code : codes) {
+            hashCode.append(code).append("-");
+        }
+        return hashCode.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> strHashMap = new HashMap<>((int) (strs.length / 0.75) + 1);
+        for (String str : strs) {
+            String strHashCode = getStrHashCode(str);
+            if (!strHashMap.containsKey(strHashCode)) {
+                strHashMap.put(strHashCode, new ArrayList<>());
+            }
+            strHashMap.get(strHashCode).add(str);
+        }
+        List<List<String>> anagramsGroup = new ArrayList<>();
+        for (HashMap.Entry<String, List<String>> entry : strHashMap.entrySet()) {
+            anagramsGroup.add(entry.getValue());
+        }
+        return anagramsGroup;
+    }
+}
