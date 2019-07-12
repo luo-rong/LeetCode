61c05fa7e6a53cbb7b63259d47d4cf37afe16429
luorong_i <luorong_i@didichuxing.com>
2019-07-12 17:39:47 +0800

    075_颜色分类

--- src/_075_SortColors/SortColors.java
+++ src/_075_SortColors/SortColors.java
@@ -2,36 +2,22 @@ package _075_SortColors;
 
 public class SortColors {
     public void sortColors(int[] nums) {
-        int i = 0, j = nums.length - 1;
-        int x = i, y = j;
-        while (i < j) {
-            while (i < j && nums[i] == 0) {
-                i++;
-                nums[x++] = 0;
+        int l = 0, r = nums.length-1;
+        for (int curr = 0; curr <= r; ) {
+            switch (nums[curr]) {
+                case 0:
+                    nums[curr++]=nums[l];
+                    nums[l++] = 0;
+                    break;
+                case 1:
+                    ++curr;
+                    break;
+                case 2:
+                    nums[curr] = nums[r];
+                    nums[r--] = 2;
+                    break;
+                default:
             }
-            while (i < j && nums[j] == 2) {
-                j--;
-                nums[y--] = 2;
-            }
-            if (nums[i] == 1 || nums[j] == 1) {
-                if (nums[j] == 1) {
-                    --j;
-                } else {
-                    ++i;
-                }
-            } else {
-                if (nums[i] == 2) {
-                    nums[x++] = 0;
-                    ++i;
-                }
-                if (nums[j] == 0) {
-                    nums[y--] = 2;
-                    --j;
-                }
-            }
-        }
-        for (int k = x; k < y; ++k) {
-            nums[k] = 1;
         }
     }
 }
